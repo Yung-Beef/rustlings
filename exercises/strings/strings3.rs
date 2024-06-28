e9109f7f3b14b4706cc9defcf9d745c10d3ae054
Yung-Beef <yungbeefbigbags@gmail.com>
2024-06-27 21:31:20 -0300

    first third of rustlings

---
+++ exercises/strings/strings3.rs
@@ -0,0 +1,61 @@
+// strings3.rs
+//
+// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+fn trim_me(input: &str) -> String {
+    // TODO: Remove whitespace from both ends of a string!
+    input.trim().to_string()
+}
+
+fn compose_me(input: &str) -> String {
+    // TODO: Add " world!" to the string! There's multiple ways to do this!
+    input.to_owned() + " world!"
+    
+}
+
+fn replace_me(input: &str) -> String {
+    // TODO: Replace "cars" in the string with "balloons"!
+    let s = input.to_owned();
+    let mut count = 0;
+    let mut f = String::new();
+    if input.len() == 0 {
+        panic!("Panic.")
+    } else {
+        for _c in s.chars() {
+            if s[count..(count+4)] == *"cars" {
+                let before: String = s[0..count].to_owned();
+                let after: String = s[(count+4)..].to_owned();
+                f = (before + "balloons" + &after).to_owned();
+            };
+            count += 1;
+        };
+    };
+    return f
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn trim_a_string() {
+        assert_eq!(trim_me("Hello!     "), "Hello!");
+        assert_eq!(trim_me("  What's up!"), "What's up!");
+        assert_eq!(trim_me("   Hola!  "), "Hola!");
+    }
+
+    #[test]
+    fn compose_a_string() {
+        assert_eq!(compose_me("Hello"), "Hello world!");
+        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+    }
+
+    #[test]
+    fn replace_a_string() {
+        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
+        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+    }
+}
